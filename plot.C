dbb98819a8aa1f7fe9aa548223169a5ccfa024da
Chroma <chroma@chroma-ThinkStation-P300.(none)>
2018-09-28 18:16:31 -0700

    minor

---
+++ plot.C
@@ -0,0 +1,23 @@
+void plot(){
+
+
+    TString filename = "test_1.root";
+    TFile* f = new TFile(filename, "read");
+    tree = (TTree*)f->Get("tree");
+
+    tree->SetAlias("r_gen", "sqrt(x_gen*x_gen+z_gen*z_gen)");
+    tree->SetAlias("track_length", "t_det*300"); // t_det * 0.3 m/ns
+    //tree->Draw("r_gen");
+    //tree->Draw("y_gen");
+    tree->Draw("track_length:r_gen", "", "colz");
+
+
+    
+
+
+    
+
+
+
+
+}
